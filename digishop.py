bb5705e3339b24ab2d3143357458929fcab0cf0a
Kevin Sánchez Obando <kesanchezo@gmail.com>
2024-09-25 17:10:35 -0600

    update to save data
    Disabled InsecureRequestWarning and saves all the information into the database

--- digishop.py
+++ digishop.py
@@ -1,5 +1,8 @@
 import requests
 import time
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 DIGI_API_URL = "https://digi-api.com/api/v1/digimon/"
 SAVE_API_URL = "https://localhost:7010/api/Digimon/digimon/save"
@@ -17,10 +20,10 @@ def fetch_digimon_data(digimon_id):
 
 def save_digimon_to_db(digimon_data):
     try:
-        response = requests.post(SAVE_API_URL, json=digimon_data)
+        response = requests.post(SAVE_API_URL, json=digimon_data, verify=False)
     
         if response.status_code == 200:
-            print(f"Successfully saved Digimon: {digimon_data['name']}")
+            print(f"Successfully saved Digimon: {digimon_data['digiName']}")
         else:
             print(f"Failed to save Digimon. Status Code: {response.status_code}")
     except Exception as e:
@@ -36,15 +39,17 @@ def automate(start_id, end_id):
                 digi_type = data["types"][0]['type']
 
             formated_data_dict = {
-                "Id": 0,
-                "Code": data["id"],
-                "DigiName": data["name"],
-                "ImageUrl": data["images"][0]['href'],
-                "MonsterType": digi_type,
-                "Price": data["id"] * 100
+                "id": 0,
+                "code": int(data["id"]),
+                "digiName": data["name"],
+                "imageUrl": data["images"][0]['href'],
+                "monsterType": digi_type,
+                "price": int(data["id"]) * 100
             }
 
-            print(formated_data_dict)
+            #print(formated_data_dict)
 
             #save into the database via API
+            save_digimon_to_db(formated_data_dict)
+
         time.sleep(1)
